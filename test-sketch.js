2fa74708bdb82705ce9a71e16240e1572d1337b8
Zehaol <97857781+Zehaol@users.noreply.github.com>
2022-01-31 01:01:21 -0700

    Update test-sketch.js

--- test-sketch.js
+++ test-sketch.js
@@ -1,120 +1,105 @@
-// Drawn from https://p5js.org/examples/simulate-penrose-tiles.html
-// This is a port by David Blitz of the "Penrose Tile" example from processing.org/examples
+// center point
+let centerX = 0.0, centerY = 0.0;
 
-var ds;
+let radius = 45, rotAngle = -90;
+let accelX = 0.0, accelY = 0.0;
+let deltaX = 0.0, deltaY = 0.0;
+let springing = 0.0009, damping = 0.98;
+
+//corner nodes
+let nodes = 5;
+
+//zero fill arrays
+let nodeStartX = [];
+let nodeStartY = [];
+let nodeX = [];
+let nodeY = [];
+let angle = [];
+let frequency = [];
+
+// soft-body dynamics
+let organicConstant = 1.0;
 
 function setup() {
   createCanvas(710, 400);
-  ds = new PenroseLSystem();
-  //please, play around with the following line
-  ds.simulate(5);
+
+  //center shape in window
+  centerX = width / 2;
+  centerY = height / 2;
+
+  //initialize arrays to 0
+  for (let i = 0; i < nodes; i++){
+    nodeStartX[i] = 0;
+    nodeStartY[i] = 0;
+    nodeY[i] = 0;
+    nodeY[i] = 0;
+    angle[i] = 0;
+  }
+
+  // iniitalize frequencies for corner nodes
+  for (let i = 0; i < nodes; i++){
+    frequency[i] = random(5, 12);
+  }
+
+  noStroke();
+  frameRate(30);
 }
 
 function draw() {
-  background(0);
-  ds.render();
+  //fade background
+  fill(0, 100);
+  rect(0, 0, width, height);
+  drawShape();
+  moveShape();
 }
 
-function PenroseLSystem() {
-    this.steps = 0;
+function drawShape() {
+  //  calculate node  starting locations
+  for (let i = 0; i < nodes; i++){
+    nodeStartX[i] = centerX + cos(radians(rotAngle)) * radius;
+    nodeStartY[i] = centerY + sin(radians(rotAngle)) * radius;
+    rotAngle += 360.0 / nodes;
+  }
 
-   //these are axiom and rules for the penrose rhombus l-system
-   //a reference would be cool, but I couldn't find a good one
-    this.axiom = "[X]++[X]++[X]++[X]++[X]";
-    this.ruleW = "YF++ZF----XF[-YF----WF]++";
-    this.ruleX = "+YF--ZF[---WF--XF]+";
-    this.ruleY = "-WF++XF[+++YF++ZF]-";
-    this.ruleZ = "--YF++++WF[+ZF++++XF]--XF";
-
-    //please play around with the following two lines
-    this.startLength = 460.0;
-    this.theta = TWO_PI / 10.0; //36 degrees, try TWO_PI / 6.0, ...
-    this.reset();
+  // draw polygon
+  curveTightness(organicConstant);
+  fill(255);
+  beginShape();
+  for (let i = 0; i < nodes; i++){
+    curveVertex(nodeX[i], nodeY[i]);
+  }
+  for (let i = 0; i < nodes-1; i++){
+    curveVertex(nodeX[i], nodeY[i]);
+  }
+  endShape(CLOSE);
 }
 
-PenroseLSystem.prototype.simulate = function (gen) {
-  while (this.getAge() < gen) {
-    this.iterate(this.production);
+function moveShape() {
+  //move center point
+  deltaX = mouseX - centerX;
+  deltaY = mouseY - centerY;
+
+  // create springing effect
+  deltaX *= springing;
+  deltaY *= springing;
+  accelX += deltaX;
+  accelY += deltaY;
+
+  // move predator's center
+  centerX += accelX;
+  centerY += accelY;
+
+  // slow down springing
+  accelX *= damping;
+  accelY *= damping;
+
+  // change curve tightness
+  organicConstant = 1 - ((abs(accelX) + abs(accelY)) * 0.1);
+
+  //move nodes
+  for (let i = 0; i < nodes; i++){
+    nodeX[i] = nodeStartX[i] + sin(radians(angle[i])) * (accelX * 2);
+    nodeY[i] = nodeStartY[i] + sin(radians(angle[i])) * (accelY * 2);
+    angle[i] += frequency[i];
   }
 }
-
-PenroseLSystem.prototype.reset = function () {
-    this.production = this.axiom;
-    this.drawLength = this.startLength;
-    this.generations = 0;
-  }
-
-PenroseLSystem.prototype.getAge = function () {
-    return this.generations;
-  }
-
-//apply substitution rules to create new iteration of production string
-PenroseLSystem.prototype.iterate = function() {
-    var newProduction = "";
-
-    for(var i=0; i<this.production.length; ++i) {
-      var step = this.production.charAt(i);
-      //if current character is 'W', replace current character
-      //by corresponding rule
-      if (step == 'W') {
-        newProduction = newProduction + this.ruleW;
-      }
-      else if (step == 'X') {
-        newProduction = newProduction + this.ruleX;
-      }
-      else if (step == 'Y') {
-        newProduction = newProduction + this.ruleY;
-      }
-      else if (step == 'Z') {
-        newProduction = newProduction + this.ruleZ;
-      }
-      else {
-        //drop all 'F' characters, don't touch other
-        //characters (i.e. '+', '-', '[', ']'
-        if (step != 'F') {
-          newProduction = newProduction + step;
-        }
-      }
-    }
-
-    this.drawLength = this.drawLength * 0.5;
-    this.generations++;
-    this.production = newProduction;
-}
-
-//convert production string to a turtle graphic
-PenroseLSystem.prototype.render = function () {
-    translate(width/2, height/2);
-
-    this.steps += 20;
-    if(this.steps > this.production.length) {
-      this.steps = this.production.length;
-    }
-
-    for(var i=0; i<this.steps; ++i) {
-      var step = this.production.charAt(i);
-
-      //'W', 'X', 'Y', 'Z' symbols don't actually correspond to a turtle action
-      if( step == 'F') {
-        stroke(255, 60);
-        for(var j=0; j < this.repeats; j++) {
-          line(0, 0, 0, -this.drawLength);
-          noFill();
-          translate(0, -this.drawLength);
-        }
-        this.repeats = 1;
-      }
-      else if (step == '+') {
-        rotate(this.theta);
-      }
-      else if (step == '-') {
-        rotate(-this.theta);
-      }
-      else if (step == '[') {
-        push();
-      }
-      else if (step == ']') {
-        pop();
-      }
-    }
-  }
